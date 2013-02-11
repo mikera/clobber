9faa16d4b17940980a55b3fbcc6d8ffb9c9a8cac
mikera <mike.r.anderson.13@gmail.com>
2013-02-11 10:43:10 +0800

    Add cljunit test

---
+++ src/main/java/mikera/clobber/TestClobber.java
@@ -0,0 +1,11 @@
+package mikera.clobber;
+
+import mikera.cljunit.ClojureTest;
+
+public class TestClobber extends ClojureTest {
+	@Override
+	public String filter () {
+		return "mikera.clobber";
+	}
+
+}
