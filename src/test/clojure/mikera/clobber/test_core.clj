67e7d44d57599f32d46ba45639348f7570426bc4
mikera <mike.r.anderson.13@gmail.com>
2013-02-11 12:01:56 +0800

    Test null handler defaults

--- src/test/clojure/mikera/clobber/test_core.clj
+++ src/test/clojure/mikera/clobber/test_core.clj
@@ -5,9 +5,15 @@
 
 (defprofn keyc [obj] :default (count (keys obj)))
 
+(defprofn nullc [obj] :null 1)
+
 (deftest test-key-count
   (is (== 3 (keyc {:a 1 :b 2 :c 3})))
   (is (thrown? Throwable (keyc nil)))
   (let [ob {:keyc (fn [& args] "foo!")}]
     (= "foo!" (keyc ob))))
+
+(deftest test-nullc
+  (is (== 1 (nullc nil)))
+  (is (thrown? Throwable (nullc {:foo :bar}))))
 
